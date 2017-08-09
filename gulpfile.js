12fe6eb4c334cb441b1ca71558a419abd242e0ea
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-08-09 10:38:41 -0400

    chore(gulp): Add project-specific tasks.

--- gulpfile.js
+++ gulpfile.js
@@ -6,10 +6,34 @@
 // Vendor
 var gulp = require( 'gulp' );
 var sass = require( 'gulp-sass' );
+var PathMap = require( 'sfco-path-map' );
 
 // --------------------------------------------------
 // DECLARE VARS
 // --------------------------------------------------
+var PATHS = new PathMap( {
+	src: './src',
+	srcStyles: '{{src}}/styles',
+	srcScripts: '{{src}}/scripts',
+	srcAssets: '{{src}}/assets',
+	dist: './dist',
+	distStyles: '{{dist}}/css',
+	distScripts: '{{dist}}/js',
+	distAssets: '{{dist}}/assets',
+} );
+
+// --------------------------------------------------
+// DEFINE FUNCTIONS
+// --------------------------------------------------
+/**
+ * Given 2x path patterns, function copies the matched files/folders from one location to the other.
+ *
+ * @param {string} from
+ * @param {string} to
+ */
+function migrate( from, to ) {
+	gulp.src( from ).pipe( gulp.dest( to ) );
+}
 
 // --------------------------------------------------
 // DEFINE TASKS
@@ -17,7 +41,7 @@ var sass = require( 'gulp-sass' );
 /**
  * Wrapper around any/all tasks to be executed when `gulp` is run.
  */
-gulp.task( 'default', [ 'styles', 'scripts' ], function() {
+gulp.task( 'default', [ 'styles', 'scripts', 'views', 'assets', 'manifest' ], function() {
 	console.log( 'INSIDE TASK: `default`' );
 } );
 
@@ -25,12 +49,37 @@ gulp.task( 'default', [ 'styles', 'scripts' ], function() {
  * Wrapper around any/all style-related tasks.
  */
 gulp.task( 'styles', function() {
-	console.log( 'INSIDE TASK: `styles`' );
+	gulp.src( `${PATHS.srcStyles}/styles.scss` )
+		.pipe( sass( {
+			outputStyle: 'expanded',
+		} ) )
+		.pipe( gulp.dest( PATHS.distStyles ) );
 } );
 
 /**
  * Wrapper around any/all script-related tasks.
  */
 gulp.task( 'scripts', function() {
-	console.log( 'INSIDE TASK: `scripts`' );
+	migrate( `${PATHS.srcScripts}/**/*.js`, PATHS.distScripts );
+} );
+
+/**
+ * Wrapper around any/all 'view'-related tasks.
+ */
+gulp.task( 'views', function() {
+	migrate( `${PATHS.src}/**/*.html`, PATHS.dist );
+} );
+
+/**
+ * Wrapper around any/all asset-related tasks.
+ */
+gulp.task( 'assets', function() {
+	migrate( `${PATHS.srcAssets}/*`, PATHS.distAssets );
+} );
+
+/**
+ * Wrapper around any/all 'manifest'-related tasks.
+ */
+gulp.task( 'manifest', function() {
+	migrate( `${PATHS.src}/manifest.json`, PATHS.dist );
 } );
